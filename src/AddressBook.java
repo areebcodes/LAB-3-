760556f5765f76f4301eb376ab5c43e6e0b8bb3f
Areeb <areebulhaq@cmail.carleton.ca>
2020-10-02 10:47:32 -0400

    change

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,12 +17,27 @@ public class AddressBook {
 
         }
         return null;
+
+        public boolean itsTrue() {
+            return true;
+        }
+
+        public boolean itsFalse() {
+            return false;
+        }
+        public boolean itsse() {
+            return false;
+        }
+
+
+    }
     }
     public static void main(String[] args){
         BuddyInfo buddy = new BuddyInfo("Tom","Carleton","613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(0);
+
 
 
     }
